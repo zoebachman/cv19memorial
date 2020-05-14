1be990211a0688b5bd48d6d4e0ebd870b796fb56
Zoe <zoe.bachman@gmail.com>
2020-05-14 00:33:51 -0400

    added read of json data in request

--- project/start.js
+++ project/start.js
@@ -3,6 +3,7 @@
 
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,26 +13,39 @@ app.use(express.static(__dirname));
 app.set('views', __dirname + '/html');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.render('pages/index');
 });
 
-app.get('/about', function(request, response) {
+app.get('/about', function (request, response) {
   response.render('pages/about');
 });
 
-app.get('/form', function(request, response) {
+app.get('/form', function (request, response) {
   response.render('pages/form');
 });
 
-app.get('/healingvoices', function(request, response) {
-  response.render('pages/healingvoices');
+app.get('/healingvoices', function (request, response) {
+  // load healing-voices.json
+  var content = fs.readFileSync('data/healing-voices.json');
+  var hvItems = JSON.parse(content);
+  hvItems = hvItems.items;
+  response.render('pages/healingvoices', {
+    hvItems: hvItems
+  });
 });
 
-app.get('/media', function(request, response) {
-  response.render('pages/media');
+app.get('/media', function (request, response) {
+  // load media.json
+  var content = fs.readFileSync('data/media.json');
+  var mediaItems = JSON.parse(content);
+  mediaItems = mediaItems.items;
+  response.render('pages/media', {
+    mediaItems: mediaItems
+  });
+  console.log(mediaItems);
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
